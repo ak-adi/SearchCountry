9c3c4f83ad63d3fc88451ff69dca5db0793d821f
Yachika Joshi <yachika@obvious.in>
2023-04-02 20:41:17 +0530

    Create retrofit service

--- app/src/main/java/com/example/countrysearch/MainActivity.kt
+++ app/src/main/java/com/example/countrysearch/MainActivity.kt
@@ -1,35 +1,40 @@
 package com.example.countrysearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.countrysearch.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val retro=  RetrofitCountry()
-       val call:Call<CountryResponse> = retro.countryAPI.getNationality("")
-        call.enqueue(object : Callback<CountryResponse>{
-            override fun onResponse(
-                call: Call<CountryResponse>,
-                response: Response<CountryResponse>
-            ) {
-                val responseBody = response.body()
-                Log.d("ajsdghfa",responseBody!!.name)
-            }
+  private lateinit var binding: ActivityMainBinding
 
-            override fun onFailure(call: Call<CountryResponse>, t: Throwable) {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    val call: Call<CountryResponse> = RetrofitCountry().getInstance()!!.countryAPI.getNationality("yachika")
 
-                Log.d("ajsdghfa",t.message.toString())
-            }
+    binding.button.setOnClickListener {
+      call.enqueue(
+        object : Callback<CountryResponse> {
+          override fun onResponse(
+            call: Call<CountryResponse>,
+            response: Response<CountryResponse>
+          ) {
+            val responseBody = response.body()
+            Log.d("ajsdghfa", responseBody!!.name)
+          }
 
+          override fun onFailure(call: Call<CountryResponse>, t: Throwable) {
+
+            Log.d("ajsdghfa", t.message.toString())
+          }
         })
 
-
-
     }
+
+  }
 }
--- app/src/main/java/com/example/countrysearch/RetrofitCountry.kt
+++ app/src/main/java/com/example/countrysearch/RetrofitCountry.kt
@@ -1,12 +1,25 @@
 package com.example.countrysearch
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitCountry {
+
+open class RetrofitCountry {
+    private var instance: RetrofitCountry? = null
+    private lateinit var myApi: CountryAPI
+    private val BASE_URL="https://api.nationalize.io/"
+
+    @Synchronized
+    fun getInstance(): RetrofitCountry? {
+        if (instance == null) {
+            instance = RetrofitCountry()
+        }
+        return instance
+    }
 
     val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.nationalize.io/")
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
